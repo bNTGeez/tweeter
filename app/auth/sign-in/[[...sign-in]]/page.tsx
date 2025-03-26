552218f7ae8137504e279a3d3e7c74632fc0038a
bNTGeez <tangbenjamin123@gmail.com>
2025-03-26 11:14:55 -0700

    more bug fixes

--- app/auth/sign-in/[[...sign-in]]/page.tsx
+++ app/auth/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="h-screen flex items-center justify-center bg-slate-500">
-      <SignIn />
+      <SignIn routing="path" path="/auth/sign-in" />
     </div>
   );
 }
--- app/auth/sign-up/[[...sign-up]]/page.tsx
+++ app/auth/sign-up/[[...sign-up]]/page.tsx
@@ -3,7 +3,7 @@ import { SignUp } from "@clerk/nextjs";
 export default function Page() {
   return (
     <div className="h-screen flex items-center justify-center bg-slate-500">
-      <SignUp />;
+      <SignUp routing="path" path="/auth/sign-up" />
     </div>
   );
 }
